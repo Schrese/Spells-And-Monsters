4dc5007cb5423fd9b995b231221a541da262d004
Schrese <53787633+Schrese@users.noreply.github.com>
2019-11-26 16:58:14 -0500

    got the map working, but not for all 22 arrays

---
+++ rawr/src/Components/MonsterCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//components
+import CombineAxios from './CombineAxios';
+
+const MonsterCard = props => {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default MonsterCard;
